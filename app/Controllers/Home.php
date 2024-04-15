720d32e61fa686262c46fc096ed644365232dbce
imposglobal <rushikesh@imposglobal.com>
2024-04-15 12:08:10 +0530

    dashboard

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,21 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        $currentURL = current_url();
-        // Get the base URL
-        $baseURL = base_url();
-         // Pass the data to the view
-         $data = [
-            'currentURL' => $currentURL,
-            'baseURL' => $baseURL
-        ];
-
-        // Load the header, main view, and footer
-        echo view('dashboard/header', $data);
-        echo view('dashboard/sidebar', $data);
-        echo view('dashboard/dashboard', $data);
-        echo view('dashboard/footer', $data);
-
-        // return view('dashboard/header.php', $data);
+       
+         return view('welcome_message');
     }
 }
