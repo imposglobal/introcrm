cd1f023561401dd0c62d5ae36a2a0711286c0201
KrushnaPhad <164015177+KrushnaPhad@users.noreply.github.com>
2024-04-18 10:14:59 +0530

    edit customer

--- app/Views/customers/edit_customer.php
+++ app/Views/customers/edit_customer.php
@@ -2,7 +2,7 @@
 <?= $this->extend('layout/layout') ?>
 <!-- Define the content section -->
 <?= $this->section('content') ?>
-<title>Add Customer</title>
+<title>Edit Customer</title>
 <style>
     .drop-area {
     border: 2px dashed #ccc;
