c468b8d791b9b0d183baa685ca4be2c691ad703b
imposglobal <rushikesh@imposglobal.com>
2024-04-29 17:41:08 +0530

    session

--- app/Views/dashboard/header.php
+++ app/Views/dashboard/header.php
@@ -9,7 +9,6 @@ $login = $session->get('email');
 // Check if the session data is empty
 if(empty($login)) {
     // Redirect to a specific URL using CodeIgniter's redirect function
-    // return redirect()->to('http://www.w3schools.com');
     echo'<script>window.location.href = "/";</script>';
 }
 
