805a9cec11df2dbf3f8b544a3c91fd7623ea11ae
imposglobal <rushikesh@imposglobal.com>
2024-05-01 17:39:26 +0530

    sdsdd

--- app/Config/App.php
+++ app/Config/App.php
@@ -18,7 +18,7 @@ class App extends BaseConfig
      */
 
     //  public string $baseURL = 'http://localhost/introcrm/';
-    public string $baseURL = 'https://eco.doodlo.in/';
+    public string $baseURL = 'https://eco4.doodlo.in/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
