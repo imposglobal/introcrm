a6f31dad2184ce27670b826a137ea4a8da8045eb
KrushnaPhad <164015177+KrushnaPhad@users.noreply.github.com>
2024-05-01 13:44:08 +0530

    assigned titles to all pages

--- app/Config/App.php
+++ app/Config/App.php
@@ -18,7 +18,7 @@ class App extends BaseConfig
      */
 
      //public string $baseURL = 'http://localhost/introcrm/';
-    public string $baseURL = 'https://eco4.doodlo.in/';
+    public string $baseURL = 'https://echo4.doodlodesign.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
