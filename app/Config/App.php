037d1e90c94d95926ee539acc43657ea0efb1592
imposglobal <rushikesh@imposglobal.com>
2024-05-01 17:32:08 +0530

    local

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,8 +17,8 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
 
-     public string $baseURL = 'http://localhost/introcrm/';
-    //public string $baseURL = 'https://echo4.doodlodesign.com/';
+    //  public string $baseURL = 'http://localhost/introcrm/';
+    public string $baseURL = 'https://echo4.doodlodesign.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
