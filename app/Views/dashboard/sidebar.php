396fb7706a0700ed7fdd369264dd841ea94fb4f6
imposglobal <rushikesh@imposglobal.com>
2024-05-09 11:30:35 +0530

    laflex

--- app/Views/dashboard/sidebar.php
+++ app/Views/dashboard/sidebar.php
@@ -80,7 +80,7 @@
         </li>
         <li class="nav-item  <?php echo ($currentURL == base_url('index.php/status/LAFLEX')) ? 'active' : ''; ?>">
             <a class="nav-link" href="<?php echo base_url('/status/LAFLEX'); ?>">
-            <i class=" menu-icon mdi mdi-file-check" style="font-size:18px"></i>
+            <i class=" menu-icon mdi mdi-account-settings" style="font-size:18px"></i>
                 <span class="menu-title">LAFLEX</span>
             </a>
         </li>
