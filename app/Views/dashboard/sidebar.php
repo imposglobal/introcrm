633bcccae502cd60ab33d02c4ce5518a3d1e753e
imposglobal <rushikesh@imposglobal.com>
2024-05-09 11:13:58 +0530

    chat

--- app/Views/dashboard/sidebar.php
+++ app/Views/dashboard/sidebar.php
@@ -55,7 +55,7 @@
         <?php if( $role == 3 || $role == 1 || $role == 0){ ?>
 
         <li class="nav-item  <?php echo ($currentURL == base_url('index.php/status/Callback')) ? 'active' : ''; ?>">
-            <a class="nav-link" href="<?php echo base_url('status/Callback'); ?>">
+            <a class="nav-link" href="<?php echo base_url('status/Callback?status=Callback'); ?>">
             <i class=" menu-icon mdi mdi-headphones-box" style="font-size:18px"></i>
                 <span class="menu-title">View Callback</span>
             </a>
