f312673f0953e3baf0de448986ce447c78e8f446
imposglobal <rushikesh@imposglobal.com>
2024-06-28 15:22:46 +0530

    profile

--- app/Views/Profile/profile.php
+++ app/Views/Profile/profile.php
@@ -1,1 +1,5 @@
-<?= "hiii" ?>
+<!-- Included header sidebar & footer in layout -->
+<?= $this->extend('layout/layout') ?>
+<!-- Define the content section -->
+<?= $this->section('content') ?>
+<?= $this->endSection() ?>
